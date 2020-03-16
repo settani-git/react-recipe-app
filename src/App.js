16810b31197f454ed0953cfcf6d0392ca6311be7
roko2018 <settani-2015@hotmail.com>
2020-03-16 17:37:08 +0100

    Added asynchronos call to recipes api to get the data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const APP_ID = "0bbbdc06";
+    const APP_KEY = "7050e6d73797175fb846204ca72674f9";
+
+    const [recipes, setRecipes] = useState([]);
+
+    // Runs first time the app get mounted
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
+    async function getRecipes(mc) {
+        const response = await fetch(
+            `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        console.log(data);
+    };
+
+    return (
+        <div className="App">
+            <form className="search-form">
+                <input type="text" className="search-bar"/>
+                <button type="submit" className="search-btn">
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
